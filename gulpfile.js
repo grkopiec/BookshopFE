921c7ffad198580215e89cfe50174d0996946135
grkopiec <grkopiec@gmail.com>
2017-04-20 22:15:29 +0200

    Adds less and compile less task and also reload browser on change files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
 var pkg = require('./package.json');
 
 var paths = {
 	js: [
 	     './angular.js',
 	     './app.js'
+	],
+	less: [
+	    'less/**/*.less' 
 	]
 }
 
@@ -23,11 +28,37 @@ gulp.task('concat', function() {
 	.pipe(gulp.dest('dist/dev'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.js, ['concat']);
+gulp.task('lessDev', function() {
+	gulp.src('less/bootstrap.less')
+	.pipe(less({
+		filename: 'bootstrap.css'
+	}))
+	.pipe(gulp.dest('dist/dev')); 
 });
 
-gulp.task('prod', ['uglify']);
+gulp.task('lessProd', function() {
+	gulp.src('less/bootstrap.less')
+	.pipe(less({
+		filename: 'bootstrap.css'
+	}))
+	.pipe(gulp.dest('dist/prod')); 
+});
+
+gulp.task('watch', function() {
+	livereload.listen(35732);
+	
+	gulp.watch(paths.js, ['concat']);
+	gulp.watch(paths.less, ['lessDev']);
+	
+	gulp.watch('dist/dev/' + pkg.name + '.js', function(file) {
+		livereload.changed(file.path);
+	});
+	gulp.watch('dist/dev/bootstrap.css', function(file) {
+		livereload.changed(file.path);
+	});
+});
+
+gulp.task('prod', ['uglify', 'lessProd']);
 gulp.task('dev', ['default']);
 
-gulp.task('default', ['concat', 'watch']);
+gulp.task('default', ['concat', 'lessDev', 'watch']);
