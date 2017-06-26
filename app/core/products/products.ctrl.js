e00ead49bbc5fc16263acb2a5db001ed173f41f9
grkopiec <grkopiec@gmail.com>
2017-06-26 20:26:51 +0200

    Establishes showing product images from server

--- app/core/products/products.ctrl.js
+++ app/core/products/products.ctrl.js
@@ -1,4 +1,7 @@
 angular.module('bookshop').controller('productsController', function(categoriesFactory, productsFactory) {
 	this.categories = categoriesFactory.query();
 	this.products = productsFactory.query();
+	this.calculateImagePath = function(imagePath) {
+		return 'http://localhost:8080' + (imagePath === null ? '/bookshop/resources/noImage.png' : imagePath);
+	}
 });
