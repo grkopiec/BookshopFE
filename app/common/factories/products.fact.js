a82e3971aa2b08675d726120509533d7bb62ce39
grkopiec <grkopiec@gmail.com>
2017-07-05 21:54:50 +0200

    Adds searching products by categories

--- app/common/factories/products.fact.js
+++ app/common/factories/products.fact.js
@@ -1,3 +1,9 @@
 angular.module('bookshop').factory('productsFactory', function($resource, serverUrlValue) {
-	return $resource(serverUrlValue + '/bookshop/products/:id', {id: '@id'}, {update: {method: 'PUT'}});
+	return $resource(serverUrlValue + '/bookshop/products/:id', {
+		id: '@id',
+		query: '@query'
+	}, {
+		update: {method: 'PUT'},
+		search: {method: 'GET', params: '@query', url: serverUrlValue + '/bookshop/products/search', isArray: true}
+	});
 });
--- app/core/products/products.ctrl.js
+++ app/core/products/products.ctrl.js
@@ -1,7 +1,21 @@
 angular.module('bookshop').controller('productsController', function(categoriesFactory, productsFactory, serverUrlValue) {
 	this.categories = categoriesFactory.query();
 	this.products = productsFactory.query();
+	this.searchByCategory = function(category) {
+		this.activeCategory = category;
+		
+		//if clicked all categories then set category parameter in query to null
+		if (category === 'All categories') {
+			category = null;
+		}
+		
+		this.query.category = category;
+		this.products = productsFactory.search(this.query);
+	}
 	this.calculateImagePath = function(imagePath) {
 		return serverUrlValue + (imagePath === null ? '/bookshop/resources/noImage.png' : imagePath);
 	}
+	
+	this.query = {};
+	this.activeCategory = 'All categories';
 });
