b2f008551bcbbd3c7c4663bf8282dadd10496632
grkopiec <grkopiec@gmail.com>
2017-07-16 00:47:47 +0200

    Adds additionally product search parameters

--- app/core/products/products.ctrl.js
+++ app/core/products/products.ctrl.js
@@ -1,6 +1,9 @@
 angular.module('bookshop').controller('productsController', function(categoriesFactory, productsFactory, serverUrlValue) {
 	this.categories = categoriesFactory.query();
 	this.products = productsFactory.query();
+	this.search = function() {
+		this.products = productsFactory.search(this.query);
+	}
 	this.searchByCategory = function(category) {
 		this.activeCategory = category;
 		
@@ -9,8 +12,9 @@ angular.module('bookshop').controller('productsController', function(categoriesF
 			category = null;
 		}
 		
+		this.query = {};
 		this.query.category = category;
-		this.products = productsFactory.search(this.query);
+		this.search();
 	}
 	this.calculateImagePath = function(imagePath) {
 		return serverUrlValue + (imagePath === null ? '/bookshop/resources/noImage.png' : imagePath);
