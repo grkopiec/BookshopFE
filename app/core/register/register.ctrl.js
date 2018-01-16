9ce3955f2d87cc2a983714f090f0c1479030c50c
Grzegorz Kopiec <grkopiec@gmail.com>
2018-01-16 22:06:18 +0100

    Add possibility of register new user

--- app/core/register/register.ctrl.js
+++ app/core/register/register.ctrl.js
@@ -5,16 +5,16 @@ angular.module('bookshop').controller('registerController', function($http, $sta
 	this.init();
 	
 	this.register = function() {
-//		$http({
-//			url: serverUrlValue + '/bookshop/auth',
-//			method: 'POST',
-//			data: {
-//				username: this.registerData.username,
-//				password: this.registerData.password
-//			}
-//		}).then(function(token) {
-//			$http.defaults.headers.common['Authorization'] = 'Bearer ' + token.data.token;
-//			$state.go('home');
-//		});
+		$http({
+			url: serverUrlValue + '/bookshop/auth/register',
+			method: 'POST',
+			data: {
+				username: this.registerData.username,
+				password: this.registerData.password
+			}
+		}).then(function(token) {
+			$http.defaults.headers.common['Authorization'] = 'Bearer ' + token.data.token;
+			$state.go('home');
+		});
 	}
 });
