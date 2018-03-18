5661c5b7db061b2b84ecfed365637e0fbf5a0d4c
Grzegorz Kopiec <grkopiec@gmail.com>
2018-03-18 20:03:48 +0100

    Update edit user data function

--- app/common/factories/users.fact.js
+++ app/common/factories/users.fact.js
@@ -1,5 +1,5 @@
 angular.module('bookshop').factory('usersFactory', function($resource, serverUrlValue) {
-	return $resource(serverUrlValue + '/bookshop/users/:id', {id: '@id'}, {
+	return $resource(serverUrlValue + '/bookshop/users/:id', {}, {
 		update: {method: 'PUT'},
 		save: {method: 'POST', url: serverUrlValue + '/bookshop/users/admin'}
 	});
--- app/core/user-panel/data/user-data.ctrl.js
+++ app/core/user-panel/data/user-data.ctrl.js
@@ -1,16 +1,20 @@
 angular.module('bookshop').controller('userDataController', function(usersFactory, userService) {
 	this.init = function() {
-		this.userData = {};
 		var model = this;
-		usersFactory.get({id: userService.id}).$promise.then(function(userData) {
-			model.userData = userData;
+		usersFactory.get({id: userService.user.userId}).$promise.then(function(userDetails) {
+			model.userDetails = userDetails;
 		});
 	}
 	this.init();
 	
 	this.update = function() {
 		if (this.dataForm.$valid) {
-			usersFactory.update(userService.id, this.userData);
+			var model = this;
+			usersFactory.update({id: userService.user.userId}, this.userDetails).$promise.then(function(userDetails) {
+				model.userDetials = userDetails;
+				model.dataForm.$setPristine();
+				model.dataForm.$setUntouched();
+			});
 		}
 	}
 });
