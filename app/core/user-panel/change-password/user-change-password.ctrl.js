f2448a1a552b10a304dce142599a52488afe42d6
Grzegorz Kopiec <grkopiec@gmail.com>
2018-06-17 22:14:05 +0200

    Add function that change password

--- app/core/user-panel/change-password/user-change-password.ctrl.js
+++ app/core/user-panel/change-password/user-change-password.ctrl.js
@@ -1,8 +1,12 @@
-angular.module('bookshop').controller('userChangePasswordController', function() {
+angular.module('bookshop').controller('userChangePasswordController', function($state, userService, usersFactory) {
 	this.init = function() {
+		this.newPassword = {};
 	}
 	this.init();
 	
 	this.update = function() {
+		usersFactory.changePassword({id: userService.user.userId}, this.newPassword).$promise.then(function() {
+			$state.go('user-panel');
+		});
 	}
 });
