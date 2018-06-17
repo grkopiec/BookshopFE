6036510e6ecc73b91bef8c8e205235b2a20b527e
Grzegorz Kopiec <grkopiec@gmail.com>
2018-06-17 22:12:54 +0200

    Add resource method for changing password

--- app/common/factories/users.fact.js
+++ app/common/factories/users.fact.js
@@ -1,6 +1,7 @@
 angular.module('bookshop').factory('usersFactory', function($resource, serverUrlValue) {
 	return $resource(serverUrlValue + '/bookshop/users/:id', {}, {
 		update: {method: 'PUT'},
-		save: {method: 'POST', url: serverUrlValue + '/bookshop/users/admin'}
+		save: {method: 'POST', url: serverUrlValue + '/bookshop/users/admin'},
+		changePassword: {method: 'PATCH', url: serverUrlValue + '/bookshop/users/change-password/:id'}
 	});
 });
