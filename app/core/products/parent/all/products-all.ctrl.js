e684d623cbaff550a450989f9f08ca3bb0ca8be2
Grzegorz Kopiec <grkopiec@gmail.com>
2018-09-21 19:31:35 +0200

    Add possibility of ordering directly from product details page

--- app/core/products/parent/all/products-all.ctrl.js
+++ app/core/products/parent/all/products-all.ctrl.js
@@ -7,13 +7,12 @@ angular.module('bookshop').controller('productsAllController', function($state, 
 			category: $stateParams.category
 		};
 		this.search();
-	}
+	}	
+	this.init();
 	
 	this.search = function() {
 		this.products = productsFactory.search(this.query);
 	}
-	
-	this.init();
 	
 	this.goToProductDetails = function(product) {
 		$state.go('products.parent.details', {product: product});
--- app/core/products/parent/product-details/product-details.ctrl.js
+++ app/core/products/parent/product-details/product-details.ctrl.js
@@ -1,7 +1,17 @@
-angular.module('bookshop').controller('productDetailsController', function($stateParams, utilService) {
+angular.module('bookshop').controller('productDetailsController', function($stateParams, utilService, ordersService) {
 	this.init = function() {
 		this.utilService = utilService;
+		this.ordersService = ordersService;
 		this.product = $stateParams.product;
+		this.quantity = 1;
 	}
 	this.init();
+	
+	this.incrementQuantity = function() {
+		this.quantity++;
+	}
+	
+	this.decrementQuantity = function() {
+		this.quantity--;
+	}
 });
